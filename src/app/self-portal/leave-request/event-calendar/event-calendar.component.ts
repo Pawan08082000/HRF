f41fb2b1edae01215c8dfe2460f9110d3fb7a0e9
Priyanshi Agrawal <priyanshiagrawalccs@gmail.com>
2021-03-05 17:39:26 +0530

    PA - event-calendar

--- src/app/self-portal/leave-request/event-calendar/event-calendar.component.ts
+++ src/app/self-portal/leave-request/event-calendar/event-calendar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
-import { CalendarOptions, FullCalendarComponent } from '@fullcalendar/angular'; // useful for typechecking
+import { CalendarOptions, DateSelectArg, EventApi, EventClickArg, FullCalendarComponent } from '@fullcalendar/angular'; // useful for typechecking
 
 
 @Component({
@@ -11,19 +11,60 @@ export class EventCalendarComponent implements OnInit {
 
   @ViewChild('calendar') calendarComponent: FullCalendarComponent;
 
-  constructor(){}
+  constructor(
+    
+  ){}
 
-  ngOnInit(){}
+  ngOnInit(){
+    
+  }
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridWeek',
-    editable: false,  
-    events: [
-      { title: 'event 1', date: '2021-02-28' },
-      { title: 'event 2', date: '2021-03-02' }
-    ]
+    editable: true,  
+    headerToolbar: {
+      left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
+    selectable: true,
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this),
+    // events: []
   };
 
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        // id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  currentEvents: EventApi[] = [];
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents = events;
+    console.log(this.currentEvents)
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
+  
   handleDateClick(arg) {
     alert('date click! ' + arg.dateStr)
   }
